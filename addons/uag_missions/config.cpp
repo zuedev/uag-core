17b3a8d3d35468f26117f98945f3e79b35d73421
Alex Pooley (@zuedev) <zuedev@gmail.com>
2025-07-13 16:16:41 +0100

    update missions

--- addons/uag_missions/config.cpp
+++ addons/uag_missions/config.cpp
@@ -15,14 +15,5 @@ class CfgMissions {
         class uag_carrier_ops_altis {
             directory = "z\UAG_Core\addons\uag_missions\missions\uag_carrier_ops_altis.altis";
         };
-        class uag_destroyer_ops_altis {
-            directory = "z\UAG_Core\addons\uag_missions\missions\uag_destroyer_ops_altis.altis";
-        };
-        class uag_carrier_ops_tanoa {
-            directory = "z\UAG_Core\addons\uag_missions\missions\uag_carrier_ops_tanoa.tanoa";
-        };
-        class uag_destroyer_ops_tanoa {
-            directory = "z\UAG_Core\addons\uag_missions\missions\uag_destroyer_ops_tanoa.tanoa";
-        };
     };
 };
--- addons/uag_missions/missions/uag_destroyer_ops_altis.altis/initServer.sqf
+++
@@ -1,9 +0,0 @@
-/* initServer.sqf
- *
- * Executed only on server when mission is started.
- *
- * More info: https://community.bistudio.com/wiki/Event_Scripts#initServer.sqf
- */
-
-// spawn arsenal at respawn marker logic
-[getPosASL respawn_logic] spawn uag_fnc_createArsenal;
